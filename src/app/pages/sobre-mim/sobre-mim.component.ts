8b8018e8766407ef781a3cf3b22c08260130977b
Camila Santiago <camila.santiago026@gmail.com>
2025-11-11 12:43:52 -0300

    fix (Sobre): Ajustes no cálculo de idade

--- src/app/pages/sobre-mim/sobre-mim.component.ts
+++ src/app/pages/sobre-mim/sobre-mim.component.ts
@@ -19,11 +19,19 @@ interface Formacao {
 export class SobreMimComponent {
   idade?: string;
   dataNasc: Date = new Date(2006, 4, 2);
-  dataAtual?: Date;
+  dataAtual = new Date();
 
-  ngOnInit() {
-    this.dataAtual = new Date();
-    this.idade = ((this.dataAtual.getTime() - this.dataNasc.getTime()) / 31557600000).toFixed(0);
+  ngOnInit() { this.idade = this.calcularIdade(this.dataNasc).toString(); }
+
+  private calcularIdade(dataNasc: Date): number {
+    const hoje = new Date();
+    let idade = hoje.getFullYear() - dataNasc.getFullYear();
+    const mesAtual = hoje.getMonth();
+    const mesNasc = dataNasc.getMonth();
+
+    if (mesAtual < mesNasc || (mesAtual === mesNasc && hoje.getDate() < dataNasc.getDate())) idade--;
+
+    return idade;
   }
 
   formacoes: Formacao[] = [
